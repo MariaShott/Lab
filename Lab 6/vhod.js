e73db44d6a1a9c8b73574c558f78086e392b7a58
MariaShott <113055145+MariaShott@users.noreply.github.com>
2022-12-22 20:30:44 +0300

    Update vhod.js
    Убрала "150" и добавила document.onmuseup = null.

--- Lab 6/vhod.js
+++ Lab 6/vhod.js
@@ -1,28 +1,9 @@
-let button = document.querySelector('#voity');
-
-let vhod = document.querySelector('.vhod');
-
-let form = document.querySelector('.forma');
-
-let butt_close = document.querySelector('.close');
-
-button.addEventListener("click", function() {
-    vhod.style.display = "block";
-    content.style.pointerEvents = "none";
-  });
-
-butt_close.addEventListener("click", function() {
-    vhod.style.display = "none";
-    content.style.pointerEvents = "auto";
-  });
-
-
 let drag = form.querySelector('h3');
 
 drag.addEventListener("mousedown", function(e) {
     let pos = form.getBoundingClientRect();
-    let shiftX = e.pageX - pos.left - 150;
-    let shiftY = e.pageY - pos.top - 150;
+    let shiftX = e.pageX - pos.left;
+    let shiftY = e.pageY - pos.top;
 
     document.onmousemove = function(e) {
         form.style.left = (e.pageX - shiftX) + "px";
@@ -31,5 +12,6 @@ drag.addEventListener("mousedown", function(e) {
 
     document.onmouseup = function() {
         document.onmousemove = null;
+        document.onmouseup = null;
     }
 });
