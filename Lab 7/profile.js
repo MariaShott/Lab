6554e560904a15df0abf0857bc2dbe4b2bf49bbf
MariaShott <113055145+MariaShott@users.noreply.github.com>
2022-12-23 22:31:37 +0300

    Create profile.js

---
+++ Lab 7/profile.js
@@ -0,0 +1,23 @@
+let profile = [
+    {
+        'name': 'Пользователь 1',
+        'login': '@User1',
+        'followers': '1',
+        'follow': '1',
+        'posts': '1'
+    },
+    {
+        'name': 'Пользователь 2',
+        'login': '@User2',
+        'followers': '2',
+        'follow': '2',
+        'posts': '2'
+    },
+    {
+        'name': 'Пользователь 3',
+        'login': '@User3',
+        'followers': '3',
+        'follow': '3',
+        'posts': '3'
+    },
+];
